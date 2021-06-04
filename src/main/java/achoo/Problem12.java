423e8c93a4dc451d13cad86bb89ef34120f3c5f3
John Kelleher <84353883+JKelleher00@users.noreply.github.com>
2021-06-04 17:48:10 -0400

    Initial commit

---
+++ src/main/java/achoo/Problem12.java
@@ -0,0 +1,19 @@
+package achoo;
+import java.util.*;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solutions
+ *  Copyright 2021 John Kelleher
+ */
+public class Problem12 {
+    public static void main(String args[]){
+         Scanner a = new Scanner(System.in);
+         System.out.print("Enter the principal: ");
+         double p = a.nextDouble();
+         System.out.print("Enter the rate of interest: ");
+         double i = a.nextDouble()/(double)100;
+         System.out.print("Enter the number of years: ");
+         double yr = a.nextDouble();
+         double amt = Math.round(p*(1+i*yr)*100)/(double)100;
+         System.out.println("After "+yr+" years at "+i*100+"% interest, the investment will be worth $"+amt);
+    }
+}
